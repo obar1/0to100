70e998a8a0179be312da46fa09e48c10dc6c6a80
obar1Merger <52974043+obar1Merger@users.noreply.github.com>
2023-12-21 10:54:30 +0000

    wip

--- demo.sh
+++ demo.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-
-python -m venv .venv
+# set -x
+# python -m venv .venv || true TODO:
 . ./venv/bin/activate
+# pip install -r requirements.txt
+
+chmod +x main*.py
+
+
+export MAP_YAML_PATH=$(pwd)/map.yaml
+
+# 0to100
+cp ./zero_to_one_hundred/tests/resources/map.yaml .
+./main.py
+
+url=https://cloud.google.com/docs/
+./main.py create_section "$url"
+url=https://docs.getdbt.com/docs/introduction
+./main.py create_section "$url"
+url=https://cloud.google.com/docs/
+./main.py done_section "$url"
+
+# 0to100 safari books
+cp ./zero_to_one_hundred/tests_sb/resources/map.yaml .
+
+ 
