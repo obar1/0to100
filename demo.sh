f1eda74dd3c7709a29da26ac68d0a49b424ded42
obar1Merger <52974043+obar1Merger@users.noreply.github.com>
2023-12-21 11:00:54 +0000

    wip

--- demo.sh
+++ demo.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 function setup {
-# set -x
-# python -m venv .venv || true TODO:
-. ./venv/bin/activate
-# pip install -r requirements.txt
+    # set -x
+    # python -m venv .venv || true TODO:
+    . ./venv/bin/activate
+    # pip install -r requirements.txt
 
-chmod +x main*.py
+    chmod +x main*.py
+
+    export MAP_YAML_PATH=$(pwd)/map.yaml
 }
 
 function 0to100 {
+    cls
+    # 0to100
 
-export MAP_YAML_PATH=$(pwd)/map.yaml
+    cp ./zero_to_one_hundred/tests/resources/map.yaml .
+    ./main.py
 
-# 0to100
-cp ./zero_to_one_hundred/tests/resources/map.yaml .
-./main.py
+    url=https://cloud.google.com/docs/
+    ./main.py create_section "$url"
+    url=https://docs.getdbt.com/docs/introduction
+    ./main.py create_section "$url"
+    url=https://cloud.google.com/docs/
+    ./main.py done_section "$url"
 
-url=https://cloud.google.com/docs/
-./main.py create_section "$url"
-url=https://docs.getdbt.com/docs/introduction
-./main.py create_section "$url"
-url=https://cloud.google.com/docs/
-./main.py done_section "$url"
-
-ls -1R 0to100
+    ls -1R 0to100
 }
 
 function 0to100_sb {
-# 0to100 safari books
-cp ./zero_to_one_hundred/tests_sb/resources/map.yaml .
+    cls
+    # 0to100 safari books
+    cp ./zero_to_one_hundred/tests_sb/resources/map.yaml .
+    ./main_sb.py
 
+    url=https://learning.oreilly.com/library/view/the-pragmatic-programmer/9780135956977/
+    ./main_sb.py create_meta_book "$url"
+
+    ls -1R 9780135956977
 }
 
 setup
-0to100
+# 0to100
 0to100_sb
