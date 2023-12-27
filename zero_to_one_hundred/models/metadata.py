da586378eaadc9fde095ee72fd2b393c8e159f88
obar1 <mamatucci@gmail.com>
2023-12-27 20:39:59 +0100

    wip

--- zero_to_one_hundred/models/metadata.py
+++ zero_to_one_hundred/models/metadata.py
@@ -47,24 +47,26 @@ class Metadata:
             self.pages_tot = self.persist_fs.read_pages_tot(
                 f"{self.contents_path}/{self.isbn}.pdf"
             )
-        finally:
-            txt = """
-            "isbn":"{isbn}",
-            "url":"{url}",
-            "page_curr":"{page_curr}",
-            "pages_tot":"{pages_tot}",
-            "page_perc":"{page_perc}"
-            """.strip()
-            txt = txt.format(
-                isbn=self.isbn,
-                url=self.http_url,
-                page_curr=self.page_curr,
-                pages_tot=self.pages_tot,
-                page_perc=self.get_page_perc,
-            )
-            print(txt)
+        except Exception as e:
+            print(f"DDD issue with {e}")
 
-            self.persist_fs.write_json(self.path_json, "{" + txt + "}")
+        txt = """
+        "isbn":"{isbn}",
+        "url":"{url}",
+        "page_curr":"{page_curr}",
+        "pages_tot":"{pages_tot}",
+        "page_perc":"{page_perc}"
+        """.strip()
+        txt = txt.format(
+            isbn=self.isbn,
+            url=self.http_url,
+            page_curr=self.page_curr,
+            pages_tot=self.pages_tot,
+            page_perc=self.get_page_perc,
+        )
+        print(txt)
+
+        self.persist_fs.write_json(self.path_json, "{" + txt + "}")
 
     def read_json(self):
         lines = "{}"
