40b74462a0d532ad3349fcd23b291b698c7392b7
obar1 <mamatucci@gmail.com>
2023-12-29 15:06:33 +0100

    wip

--- zero_to_one_hundred/tests_sb/test_meta_book.py
+++ zero_to_one_hundred/tests_sb/test_meta_book.py
@@ -5,7 +5,7 @@ from zero_to_one_hundred.repository.sb_process_fs import SBProcessFS as sb_proce
 
 
 # pylint: disable=W0613
-def test_init(get_map_yaml_path, http_url):
+def test_init(get_config_map, http_url):
     actual = MetaBook(
         SBConfigMap(sb_persist_fs),
         sb_persist_fs,
@@ -19,7 +19,7 @@ def test_init(get_map_yaml_path, http_url):
     assert str(actual.path_img).endswith("9780135956977/9780135956977.png")
 
 
-def test_build_from_dir(get_map_yaml_path):
+def test_build_from_dir(get_config_map):
     assert (
         MetaBook.build_from_dir(
             SBConfigMap(sb_persist_fs),
@@ -33,5 +33,5 @@ def test_build_from_dir(get_map_yaml_path):
 
 def test_is_valid_ebook_path():
     dirs = ["0123456789", "1234567890123", "books", "ABC"]
-    actual = [dir_ for dir_ in dirs if MetaBook.is_valid_ebook_path(dir_)]
+    actual = [d for d in dirs if MetaBook.is_valid_ebook_path(d)]
     assert actual == ["1234567890123"]
