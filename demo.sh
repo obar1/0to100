f9e08ef4869378545cc3c3cc62063c703881ccc3
obar1 <387386+obar1@users.noreply.github.com>
2024-08-20 07:26:35 +0200

    114 add new sort as last opened (#115)

--- demo.sh
+++ demo.sh
@@ -14,13 +14,13 @@ function setup {
 function setup0to100 {
     rm -rf 0to100/
     
-    cp ./zero_to_one_hundred/tests/resources/gcp_map.yaml map.yaml
+    cp ./zero_to_one_hundred/tests/test_ztoh/resources/gcp_map.yaml map.yaml
 }
 
 function setup0to100_sb {
     rm -rf 978*/
     
-    cp ./zero_to_one_hundred/tests_sb/resources/map.yaml .
+    cp ./zero_to_one_hundred/tests/tests_sb/resources/map.yaml map.yaml
     
     # safari books from lorenzodifuccia
     git clone https://github.com/lorenzodifuccia/safaribooks.git
@@ -50,12 +50,12 @@ function 0to100_sb {
     setup0to100_sb
     
     ./main_sb.py help
-
+    
     ./main_sb.py snatch_book https://learning.oreilly.com/course/clean-code-fundamentals/9780134661742
     echo 'add any metadata you like'
     echo '{"title": "Clean Code Fundamentals"}'> 9780134661742/9780134661742.json
     ./main_sb.py refresh_toc
-
+    
     ./main_sb.py snatch_book https://learning.oreilly.com/library/view/rewire-your-brain/9781119895947
     echo 'pretend book was read fully and get % calc for free :P'
     echo '{"page_curr": "100", "page_tot": "100", "url":"https://www.oreilly.com/library/view/rewire-your-brain/9781119895947"}' > 9781119895947/9781119895947.json
