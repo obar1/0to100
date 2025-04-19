3cacc7089999759948c385669e99f44e5e0fb2da
obar1 <mamatucci@gmail.com>
2025-04-19 19:52:25 +0200

    wip:

--- main.py
+++ main.py
@@ -3,12 +3,29 @@
 
 import logging
 import sys
+from typing import List
 
-from zero_to_one_hundred.runner import run_core
 from zero_to_one_hundred.src.zero_to_one_hundred.exceptions.errors import UnsupportedOptionError
 from zero_to_one_hundred.src.zero_to_one_hundred.factories.sb_factory_provider import SBFactoryProvider
 from zero_to_one_hundred.src.zero_to_one_hundred.factories.ztoh_factory_provider import ZTOHFactoryProvider
 from zero_to_one_hundred.src.zero_to_one_hundred.validator.validator import Validator
+
+
+def run_core(argv: List[str], factory_provider: AFactoryProvider):
+    """given params and factory provider it runs the core logic
+
+    Args:
+        argv (List[str]): args from cmd line
+        factory_provider (AFactoryProvider): a factory_type
+
+    """
+
+    factory = factory_provider.provide()
+    if factory:
+        for processor in factory.get_processor(argv):
+            if processor:
+                processor.process()
+                
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
